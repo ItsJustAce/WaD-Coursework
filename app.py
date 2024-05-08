d268f1b2803b1782a623e83b3f13fa163b4f5d83
Tom <37934171+ItsJustAce@users.noreply.github.com>
2024-05-08 21:49:21 +0100

    convert to flask page

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route("/")
+@app.route("/index/")
+@app.route("/home/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/account/")
+def account():
+    name = "balls"
+    return render_template("account.html", name=name)
