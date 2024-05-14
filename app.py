760bb283ffcd77786b57ff54b5cd29b795257eb1
Tom <37934171+ItsJustAce@users.noreply.github.com>
2024-05-14 20:30:06 +0100

    add mysqldb functionality

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ Version 1
 WorldHotels flask website for booking and managing hotels and their rooms
 """
 
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+app = Flask(__name__)  
+
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'adminpass'
+app.config['MYSQL_DB'] = 'WaD'
+
+db = MySQL(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 @app.route("/")
 @app.route("/index/")
@@ -26,3 +40,10 @@ def login():
 def account():
     name = "balls"
     return render_template("account.html", name=name)
+
+@app.route("/DBTEST/")
+def dbtest():
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users')
+    print(cursor.fetchall())
+    return "test"
