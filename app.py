45c3788301e688674e9d12cb1f19817f6601c263
Tom <37934171+ItsJustAce@users.noreply.github.com>
2024-05-15 15:26:09 +0100

    add account registration

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ WorldHotels flask website for booking and managing hotels and their rooms
 
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
+from flask_admin import Admin
 import MySQLdb.cursors
 import re
 
@@ -29,6 +30,12 @@ def queryDB(query):
     cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute(query)
     return cursor.fetchone()
+
+def writeDB(query):
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query)
+    db.connection.commit()
+    return True
 
 #page routing
 @app.route("/")
@@ -59,11 +66,47 @@ def login():
     return render_template('login.html', msg = msg)
 
 
+#register user
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        tel = request.form['tel']
+
+        query = f'SELECT * FROM users WHERE username = "{username}"'
+        account = queryDB(query)
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not re.match(r'[0-9]', tel):
+            msg = 'Phone number must have only 11 numbers !'
+        elif not username or not password or not email or not tel:
+            msg = 'Please fill out the form !'
+        else:
+            query = f'INSERT INTO users VALUES (NULL, "{email}", "{tel}", "{password}", "{username}" )'
+            writeDB(query)
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+#redirect if not logged in
 @app.route("/account/")
 def account():
-    name = session['username']
-    return render_template("account.html", name=name)
+    if session.get('loggedin') == True:
+        name = session['username']
+        return render_template("account.html", name=name)
+    else:
+        msg = "Please log in before viewing your account."
+        return render_template('login.html', msg = msg)
 
+#remove user from session and redirect to login
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
@@ -71,7 +114,8 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-@app.route("/DBTEST/")
-def dbtest():
-    result = queryDB('SELECT * FROM users;')
-    return result[0]
+#DB testing purposes ONLY
+# @app.route("/DBTEST/")
+# def dbtest():
+#     result = queryDB('SELECT * FROM users;')
+#     return result[0]
